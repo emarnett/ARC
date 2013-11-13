833dda20504fa375b1b71b6d500bda2299630005
Emily Arnett <em_arnett@hotmail.com>
2013-11-13 15:00:20 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ARC::Application.config.secret_key_base = '616b8bcda1267e5fb19b3ca0a97faf279f99fb8e94b532fefc2942cc50b1a0517307d930972ab7cd22331497ed9b521b9db7a95255e98c5dbfe8eb80e15f8253'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ARC::Application.config.session_store :cookie_store, key: '_ARC_session'
